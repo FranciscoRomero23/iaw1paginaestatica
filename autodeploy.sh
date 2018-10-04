a9dee40d1e8f98009285691a6596887781225437
FranciscoRomero23 <fjo7o6@hotmail.com>
2018-10-04 11:09:24 +0000

    Commit

--- autodeploy.sh
+++ autodeploy.sh
@@ -4,4 +4,6 @@ jekyll build
 
 git add *
 git commit -m "Commit"
-git push
+git push origin master
+Username for 'https://github.com': FranciscoRomero23
+Password for 'https://FranciscoRomero23@github.com': Entrada25
